3149e241a5b8450ed735335b0ba63d8dc0833aa5
Vitesh Bava <vitesh.bava@gmail.com>
2021-09-26 15:12:39 +1300

    more updates

--- schemas.js
+++ schemas.js
@@ -4,20 +4,13 @@ const divebarSchema = joi.object({
   divebar: joi
     .object({
       title: joi.string().required(),
-      // capacity: joi.optional(),
-      capacity: joi.optional().allow(null),
-      // capacity: joi.number().optional().integer().min(0).allow(null),
-      // capacity: joi.number().integer().min(0),
+      capacity: joi.number().integer().required().min(0),
       description: joi.string().required(),
       location: joi.string().required(),
-      // rating: joi.number().integer().allow(null).optional(),
       image: joi.string().optional().allow(""),
       map: joi.string().optional().allow(""),
     })
     .required(),
-  // .when(joi.object({ capacity: joi.exist() }), {
-  //   then: joi.object({ capacity: joi.number().integer().min(0) }),
-  // }),
 });
 
 const reviewSchema = joi.object({
