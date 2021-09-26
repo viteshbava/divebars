73cc44f0f234d05fd8dab4955afc57ee6a45a053
Vitesh Bava <vitesh.bava@gmail.com>
2021-09-26 16:38:28 +1300

    changing around divebarExists middleware

--- middleware/divebars.js
+++ middleware/divebars.js
@@ -2,6 +2,23 @@ const { divebarSchema } = require("../schemas.js");
 const Divebar = require("../models/Divebar");
 const catchAsync = require("../utils/catchAsync");
 const ExpressError = require("../utils/ExpressError");
+const ObjectId = require("mongodb").ObjectId;
+
+const divebarExists = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+  if (!ObjectId.isValid(id)) {
+    req.flash("error", `That divebar ID is invalid! (ID: ${id})`);
+    return res.redirect("/divebars");
+  }
+  const divebar = await Divebar.findById(id);
+  if (!divebar) {
+    req.flash("error", `A divebar with that ID cannot be found! (ID: ${id})`);
+    return res.redirect("/divebars");
+  } else {
+    res.locals.divebar = divebar;
+    next();
+  }
+});
 
 const validateDivebar = (req, res, next) => {
   const { error } = divebarSchema.validate(req.body, { abortEarly: false });
@@ -13,15 +30,4 @@ const validateDivebar = (req, res, next) => {
   }
 };
 
-const findDiveBar = catchAsync(async (req, res, next) => {
-  const divebar = await Divebar.findById(id);
-  if (!divebar) {
-    req.flash("error", `A divebar with that ID cannot be found! (ID: ${id})`);
-    res.redirect("/divebars");
-  } else {
-    res.locals.divebar = divebar;
-    next();
-  }
-});
-
-module.exports = { validateDivebar };
+module.exports = { validateDivebar, divebarExists };
--- middleware/index.js
+++ middleware/index.js
@@ -1,11 +1,3 @@
-const ObjectId = require("mongodb").ObjectId;
-
-const isIdValid = (req, res, next) => {
-  const { id } = req.params;
-  if (!ObjectId.isValid(id)) {
-    req.flash("error", `That divebar ID is invalid! (ID: ${id})`);
-    return res.redirect("/divebars");
-  } else next();
-};
+const isIdValid = (req, res, next) => {};
 
 module.exports = { isIdValid };
