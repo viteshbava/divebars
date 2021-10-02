bf25d6cbfda753a4d8a595867cd25ca2070f7126
Vitesh Bava <vitesh.bava@gmail.com>
2021-10-02 19:03:53 +1300

    added isloggedin check for reviews

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,10 +2,17 @@ const express = require("express");
 const router = express.Router({ mergeParams: true });
 const { validateReview, reviewExists } = require("../middleware/reviews");
 const { divebarExists } = require("../middleware/divebars");
+const { isLoggedIn } = require("../middleware/users");
 const reviewsCtr = require("../controllers/reviews");
 
-router.post("/", validateReview, reviewsCtr.create);
+router.post("/", isLoggedIn, validateReview, reviewsCtr.create);
 
-router.delete("/:reviewId", divebarExists, reviewExists, reviewsCtr.delete);
+router.delete(
+  "/:reviewId",
+  isLoggedIn,
+  divebarExists,
+  reviewExists,
+  reviewsCtr.delete
+);
 
 module.exports = router;
