f04f54fc153294be6386bea09d5b17bb0025d017
Vitesh Bava <vitesh.bava@gmail.com>
2021-10-14 17:59:41 +1300

    took out config from path in app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config({ path: "./config/config.env" });
+  require("dotenv").config({ path: "./config/.env" });
 }
 
 const express = require("express");
