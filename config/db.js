79339399230d8615b581d2ae84add9abd601162c
Vitesh Bava <vitesh.bava@gmail.com>
2021-10-20 11:12:36 +1300

    still on lesson 569 mongo session

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     // connect to db
-    const conn = await mongoose.connect(process.env.MONGO_URI_DEV);
+    const conn = await mongoose.connect(process.env.MONGO_URI_PROD);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.error(err);
--- config/sessionConfig.js
+++ config/sessionConfig.js
@@ -1,4 +1,19 @@
+const MongoStore = require("connect-mongo");
+
+const store = MongoStore.create({
+  mongoUrl: process.env.MONGO_URI_DEV,
+  touchAfter: 24 * 60 * 60,
+  crypto: {
+    secret: process.env.MONGOSTORE_SECRET,
+  },
+});
+
+store.on("error", function (e) {
+  console.error("SESSION STORE ERROR", e);
+});
+
 module.exports = {
+  store,
   name: "dvbrsssn",
   secret: process.env.SESSION_SECRET,
   resave: false,
