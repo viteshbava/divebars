b0ffb1a9f53b6bb65487e5e34455f4c40d6bb744
Vitesh Bava <vitesh.bava@gmail.com>
2021-10-20 15:29:10 +1300

    about to start deployment to Heroku

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const morgan = require("morgan");
-const connectDB = require("./config/db");
+require("./config/db")();
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError");
@@ -25,7 +25,6 @@ const userRoutes = require("./routes/users");
 const indexRoutes = require("./routes");
 const errorRoute = require("./routes/error");
 
-connectDB();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 app.engine("ejs", ejsMate);
--- config/db.js
+++ config/db.js
@@ -3,10 +3,11 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     // connect to db
-    const conn = await mongoose.connect(process.env.MONGO_URI_PROD);
+    console.log("Connecting to MongoDB...");
+    const conn = await mongoose.connect(process.env.MONGO_URI_DEV);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
-    console.error(err);
+    console.error("Failed to connect ot MongoDB!", err);
     process.exit(1);
   }
 };
